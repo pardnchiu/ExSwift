e41408adfe75bb64197111ca014237c9e8613595
pardnchiu <dev@pardn.io>
2024-12-10 23:46:03 +0800

    202412102346

---
+++ ExSwift.xcframework/ios-arm64/ExSwift.framework/Headers/ExSwift.h
@@ -0,0 +1,18 @@
+//
+//  ExSwift.h
+//  ExSwift
+//
+//  Created by Pardn on 2024/12/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExSwift.
+FOUNDATION_EXPORT double ExSwiftVersionNumber;
+
+//! Project version string for ExSwift.
+FOUNDATION_EXPORT const unsigned char ExSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExSwift/PublicHeader.h>
+
+
